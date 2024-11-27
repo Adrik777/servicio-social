168c10a9573769559d95b1116e686509ac20509b
adrikk <adrik.duran@gmail.com>
2024-11-26 17:39:04 -0800

    Creacion de membresia page

--- frontend/src/Pages/Equipo/Equipo.tsx
+++ frontend/src/Pages/Equipo/Equipo.tsx
@@ -1,8 +1,65 @@
+import "./Equipo.css";
+
 export default function Beca() {
-    return(
-        <div>
-            <h1>Equipo</h1>
-        </div>
-    )
-    
+  const teamData = [
+    {
+      name: "CEO Name",
+      role: "CEO",
+      bio: "Short biography of the CEO.",
+      image: "/assets/user.png",
+    },
+    {
+      name: "Head of Department 1",
+      role: "Head of Department",
+      bio: "Short biography of the head of department.",
+      image: "/assets/user.png",
+    },
+    {
+      name: "Head of Department 2",
+      role: "Head of Department",
+      bio: "Short biography of the head of department.",
+      image: "/assets/user.png",
+    },
+    {
+      name: "Team Lead 1",
+      role: "Team Lead",
+      bio: "Short biography of the team lead.",
+      image: "/assets/user.png",
+    },
+    {
+      name: "Team Lead 2",
+      role: "Team Lead",
+      bio: "Short biography of the team lead.",
+      image: "/assets/user.png",
+    },
+    {
+      name: "Team Lead 3",
+      role: "Team Lead",
+      bio: "Short biography of the team lead.",
+      image: "/assets/user.png",
+    },
+    {
+      name: "Team Lead 4",
+      role: "Team Lead",
+      bio: "Short biography of the team lead.",
+      image: "/assets/user.png",
+    },
+  ];
+
+  return (
+    <div id="equipo">
+      <h1>Nuestro Equipo</h1>
+      <p>Conoce a los líderes de nuestra organización.</p>
+      <div id="personas">
+        {teamData.map((person, index) => (
+          <div key={index} className="card">
+            <img src={person.image} alt={`${person.name}'s profile`} />
+            <h3>{person.name}</h3>
+            <p>{person.role}</p>
+            <p>{person.bio}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
